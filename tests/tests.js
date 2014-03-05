c3e08d705df91ee96629b08782648e36dcaf67f0
samsur <Samdup.surkhang@gmail.com>
2014-03-04 21:35:14 -0500

    first demo-node application

---
+++ tests/tests.js
@@ -0,0 +1,16 @@
+/**
+ * Created by samdup on 04/03/14.
+ */
+
+var should = require('should');
+var assert = require('assert');
+
+
+describe('Test Framework', function () {
+    it('should have mocha installed and running', function () {
+        assert.equal(true, true);
+    })
+    it('should have the should library installed and running for fluent testing', function () {
+        true.should.eql(true);
+    })
+})
